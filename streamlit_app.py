17d5ac7d138ffc718f0520826d28b704203c98e2
fenil <fenilmodi4820@gmail.com>
2024-09-03 22:56:41 +0530

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,197 @@
+import replicate
+import streamlit as st
+import requests
+import zipfile
+import io
+from utils import icon
+from streamlit_image_select import image_select
+
+# UI configurations
+st.set_page_config(page_title="Replicate Image Generator",
+                   page_icon=":bridge_at_night:",
+                   layout="wide")
+icon.show_icon(":foggy:")
+st.markdown("# :rainbow[Text-to-Image Artistry Studio]")
+
+# API Tokens and endpoints from `.streamlit/secrets.toml` file
+REPLICATE_API_TOKEN = st.secrets["general"]["REPLICATE_API_TOKEN"]
+REPLICATE_MODEL_ENDPOINT = st.secrets["general"]["REPLICATE_MODEL_ENDPOINT"]
+
+# Resources text, link, and logo
+replicate_text = "Flux Schnell Model on Replicate"
+replicate_link = "https://replicate.com/black-forest-labs/flux-schnell"
+replicate_logo = "https://storage.googleapis.com/llama2_release/Screen%20Shot%202023-07-21%20at%2012.34.05%20PM.png"
+
+# Placeholders for images and gallery
+generated_images_placeholder = st.empty()
+gallery_placeholder = st.empty()
+
+
+def configure_sidebar() -> None:
+    """
+    Setup and display the sidebar elements.
+
+    This function configures the sidebar of the Streamlit application, 
+    including the form for user inputs and the resources section.
+    """
+    with st.sidebar:
+        with st.form("my_form"):
+            st.info("**Yo fam! Start here ↓**", icon="👋🏾")
+            with st.expander(":rainbow[**Refine your output here**]"):
+                # Advanced Settings (for the curious minds!)
+                aspect_ratio = st.selectbox(
+                    "Aspect Ratio", ("1:1", "16:9", "4:3", "3:2"))
+                num_outputs = st.slider(
+                    "Number of images to output", value=1, min_value=1, max_value=4)
+                output_format = st.selectbox(
+                    "Output Format", ("webp", "png", "jpeg"))
+                output_quality = st.slider(
+                    "Output Quality", value=80, min_value=1, max_value=100)
+            prompt = st.text_area(
+                ":orange[**Enter prompt: start typing, Shakespeare ✍🏾**]",
+                value="An astronaut riding a rainbow unicorn, cinematic, dramatic")
+            negative_prompt = st.text_area(":orange[**Party poopers you don't want in image? 🙅🏽‍♂️**]",
+                                           value="the absolute worst quality, distorted features",
+                                           help="This is a negative prompt, basically type what you don't want to see in the generated image")
+
+            # The Big Red "Submit" Button!
+            submitted = st.form_submit_button(
+                "Submit", type="primary", use_container_width=True)
+
+        # Credits and resources
+        st.divider()
+        st.markdown(
+            ":orange[**Resources:**]  \n"
+            f"<img src='{replicate_logo}' style='height: 1em'> [{replicate_text}]({replicate_link})",
+            unsafe_allow_html=True
+        )
+        st.markdown(
+            """
+            ---
+            Follow me on:
+
+            𝕏 → [@tonykipkemboi](https://twitter.com/tonykipkemboi)
+
+            LinkedIn → [Tony Kipkemboi](https://www.linkedin.com/in/tonykipkemboi)
+
+            """
+        )
+
+        return submitted, aspect_ratio, num_outputs, output_format, output_quality, prompt, negative_prompt
+
+
+def main_page(submitted: bool, aspect_ratio: str, num_outputs: int,
+              output_format: str, output_quality: int,
+              prompt: str, negative_prompt: str) -> None:
+    """Main page layout and logic for generating images.
+
+    Args:
+        submitted (bool): Flag indicating whether the form has been submitted.
+        aspect_ratio (str): Aspect ratio of the output image.
+        num_outputs (int): Number of images to output.
+        output_format (str): Format of the output image.
+        output_quality (int): Quality of the output image.
+        prompt (str): Text prompt for the image generation.
+        negative_prompt (str): Text prompt for elements to avoid in the image.
+    """
+    if submitted:
+        with st.status('👩🏾‍🍳 Whipping up your words into art...', expanded=True) as status:
+            st.write("⚙️ Model initiated")
+            st.write("🙆‍♀️ Stand up and stretch in the meantime")
+            try:
+                # Only call the API if the "Submit" button was pressed
+                if submitted:
+                    # Calling the replicate API to get the image
+                    with generated_images_placeholder.container():
+                        all_images = []  # List to store all generated images
+                        output = replicate.run(
+                            REPLICATE_MODEL_ENDPOINT,
+                            input={
+                                "prompt": prompt,
+                                "num_outputs": num_outputs,
+                                "aspect_ratio": aspect_ratio,
+                                "output_format": output_format,
+                                "output_quality": output_quality
+                            }
+                        )
+                        if output:
+                            st.toast(
+                                'Your image has been generated!', icon='😍')
+                            # Save generated image to session state
+                            st.session_state.generated_image = output
+
+                            # Displaying the image
+                            for image in st.session_state.generated_image:
+                                with st.container():
+                                    st.image(image, caption="Generated Image 🎈",
+                                             use_column_width=True)
+                                    # Add image to the list
+                                    all_images.append(image)
+
+                                    response = requests.get(image)
+                        # Save all generated images to session state
+                        st.session_state.all_images = all_images
+
+                        # Create a BytesIO object
+                        zip_io = io.BytesIO()
+
+                        # Download option for each image
+                        with zipfile.ZipFile(zip_io, 'w') as zipf:
+                            for i, image in enumerate(st.session_state.all_images):
+                                response = requests.get(image)
+                                if response.status_code == 200:
+                                    image_data = response.content
+                                    # Write each image to the zip file with a name
+                                    zipf.writestr(
+                                        f"output_file_{i+1}.{output_format}", image_data)
+                                else:
+                                    st.error(
+                                        f"Failed to fetch image {i+1} from {image}. Error code: {response.status_code}", icon="🚨")
+                        # Create a download button for the zip file
+                        st.download_button(
+                            ":red[**Download All Images**]", data=zip_io.getvalue(), file_name="output_files.zip", mime="application/zip", use_container_width=True)
+                status.update(label="✅ Images generated!",
+                              state="complete", expanded=False)
+            except Exception as e:
+                print(e)
+                st.error(f'Encountered an error: {e}', icon="🚨")
+
+    # If not submitted, chill here 🍹
+    else:
+        pass
+
+    # Gallery display for inspo
+    with gallery_placeholder.container():
+        img = image_select(
+            label="Like what you see? Right-click and save! It's not stealing if we're sharing! 😉",
+            images=[
+                "gallery/farmer_sunset.png", "gallery/astro_on_unicorn.png",
+                "gallery/friends.png", "gallery/wizard.png", "gallery/puppy.png",
+                "gallery/cheetah.png", "gallery/viking.png",
+            ],
+            captions=["A farmer tilling a farm with a tractor during sunset, cinematic, dramatic",
+                      "An astronaut riding a rainbow unicorn, cinematic, dramatic",
+                      "A group of friends laughing and dancing at a music festival, joyful atmosphere, 35mm film photography",
+                      "A wizard casting a spell, intense magical energy glowing from his hands, extremely detailed fantasy illustration",
+                      "A cute puppy playing in a field of flowers, shallow depth of field, Canon photography",
+                      "A cheetah mother nurses her cubs in the tall grass of the Serengeti. The early morning sun beams down through the grass. National Geographic photography by Frans Lanting",
+                      "A close-up portrait of a bearded viking warrior in a horned helmet. He stares intensely into the distance while holding a battle axe. Dramatic mood lighting, digital oil painting",
+                      ],
+            use_container_width=True
+        )
+
+
+def main():
+    """
+    Main function to run the Streamlit application.
+
+    This function initializes the sidebar configuration and the main page layout.
+    It retrieves the user inputs from the sidebar, and passes them to the main page function.
+    The main page function then generates images based on these inputs.
+    """
+    submitted, aspect_ratio, num_outputs, output_format, output_quality, prompt, negative_prompt = configure_sidebar()
+    main_page(submitted, aspect_ratio, num_outputs, output_format, output_quality, prompt, negative_prompt)
+
+
+if __name__ == "__main__":
+    main()
